bb57bc3ed30527476788952f0cd9beb5b7779ff3
Jannick Stephan Fahlbusch <git@jf-projects.de>
2023-03-10 22:17:50 +0100

    Minor fixes

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -15,15 +15,13 @@ func (handler *LiveDataHandler) ServeHTTP(rw http.ResponseWriter, request *http.
 	defer request.Body.Close()
 	decoder := json.NewDecoder(request.Body)
 	var data *types.LiveData
-	err := decoder.Decode(data)
+	err := decoder.Decode(&data)
 	if err != nil {
 		rw.WriteHeader(http.StatusBadRequest)
 		log.Println(err)
 	}
 
-	rw.WriteHeader(http.StatusOK)
-
 	metrics.Speed.WithLabelValues().Set(data.Speed)
 	metrics.StateOfCharge.WithLabelValues(fmt.Sprintf("%t", data.IsCharging), fmt.Sprintf("%t", data.IsParked)).Set(data.StateOfCharge)
-	metrics.StateOfCharge.WithLabelValues(fmt.Sprintf("%t", data.IsCharging), fmt.Sprintf("%t", data.IsParked)).Set(data.Power)
+	metrics.Power.WithLabelValues(fmt.Sprintf("%t", data.IsCharging), fmt.Sprintf("%t", data.IsParked)).Set(data.Power)
 }
